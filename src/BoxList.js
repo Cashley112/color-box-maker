3a7971e40a7cdb41db8f1ebc60ce3ea78461f8ce
Connor Ashley <ashleycm@vcu.edu>
2021-06-24 17:00:36 -0400

    added BoxForm to render

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import BoxForm from './BoxForm';
 
 class BoxList extends Component {
     render () {
         return (
             <div>
                 <h1>Color Box Maker</h1>
+                <BoxForm />
                 <Box />
             </div>
         )
