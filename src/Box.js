184f7eaeb3c8e40cf04607a0f947e9c1d083a798
Connor Ashley <ashleycm@vcu.edu>
2021-06-23 23:47:10 -0400

    basic boilerplate for child component

---
+++ src/Box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Box.css';
+
+class Box extends Component {
+    render () {
+        return (
+            <div className="Box">
+
+            </div>
+        )
+    }
+}
+
+export default Box;
