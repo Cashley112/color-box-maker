b55037af5d17575fae202c4eecd5a42f0bbba1a3
Connor Ashley <ashleycm@vcu.edu>
2021-06-25 18:49:56 -0400

    inherit removeBox() from parent, call and pass in boxId for reference in parent

--- src/Box.js
+++ src/Box.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './Box.css';
 
 class Box extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
     static defaultProps = {
         boxStyles: { height: '50px', width: '50px', backgroundColor: 'purple' }
+    }
+    handleClick() {
+        this.props.removeBox(this.props.boxStyles.box.id);
     }
     render () {
         return (
             <div className="Box" style={this.props.boxStyles.box}>
-
+                <button style={{backgroundColor: this.props.boxStyles.box.backgroundColor}} onClick={this.handleClick}>x</button>
             </div>
         )
     }
