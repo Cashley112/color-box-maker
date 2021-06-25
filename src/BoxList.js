dfebd30ecc39a8760c17090d45f996572a03ee57
Connor Ashley <ashleycm@vcu.edu>
2021-06-24 22:18:59 -0400

    bound addBox method taht takes an argument from the BoxForm and spreads it into a new this.state.boxArray

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import Box from './Box';
 import BoxForm from './BoxForm';
+import { v4 as uuidv4 } from 'uuid';
 
 class BoxList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxArray: []
+        };
+        this.addBox = this.addBox.bind(this);
+    }
+    addBox(box) {
+        this.setState(state => ({
+            boxArray: [...state.boxArray, {...box, id: uuidv4()}]
+        }))
+    }
     render () {
         return (
             <div>
                 <h1>Color Box Maker</h1>
-                <BoxForm />
-                <Box />
+                <BoxForm 
+                    addBox={this.addBox}
+                />
+                {this.state.boxArray.map(box => (
+                    <Box boxStyles={{box}}/>
+                ))}
             </div>
         )
     }
