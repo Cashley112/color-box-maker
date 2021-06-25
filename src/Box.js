72d60c44c7cea5eb9fc372cf93b04f6335f82ff9
Connor Ashley <ashleycm@vcu.edu>
2021-06-24 22:16:00 -0400

    inherits an object of style key/value pairs and calls them with the inline style attribute

--- src/Box.js
+++ src/Box.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import './Box.css';
 
 class Box extends Component {
+    static defaultProps = {
+        boxStyles: { height: '50px', width: '50px', backgroundColor: 'purple' }
+    }
     render () {
         return (
-            <div className="Box">
+            <div className="Box" style={this.props.boxStyles.box}>
 
             </div>
         )
